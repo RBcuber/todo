ce64c4122a99a2b6405ad442d1b956fbb5dbad74
RBcuber <igor.kirichenko.07@gmail.com>
2022-02-21 20:50:56 +0200

    update all files

--- js/main.js
+++ js/main.js
@@ -6,39 +6,43 @@ const list = document.querySelector('#todo__list');
 let test;
 let amountItem = 0;
 
-form.addEventListener('submit', qwe);
-
-function qwe(e) {
-	e.preventDefault();
-	if (input.value.length) {
-			list.innerHTML += `
-            <div class="todo__task-item">
+const qwerty = (params) => (`
+            <div class="todo__item">
 						
-                <div id="todo__task-name">
-								<i class="fa-regular fa-square sss"></i>
-                    ${input.value}
+                <div id="todo">
+								<i class="fa-regular fa-square sss checkbox"></i>
+                    ${params}
                 </div>
 								 <div class="todo__delete-item">
 									 <i class="fa-solid fa-xmark"></i>
 								</div>	
             </div>
-	        `;
-			input.value = '';
-			counterItem.innerHTML = ++amountItem;
+	        `)
+
+form.addEventListener('submit', qwe);
+
+function qwe(e) {
+	e.preventDefault();
+
+	if (input.value.length) {
+		list.innerHTML += qwerty(input.value);
+		input.value = '';
+		counterItem.innerHTML = ++amountItem;
 	}
+
 	let current_tasks = document.querySelectorAll('.todo__delete-item');
-	let fa_square = document.querySelectorAll('.fa-square');
+	let fa_square = document.querySelectorAll('.checkbox');
+
 	for (let i = 0; i < current_tasks.length; i++) {
 		current_tasks[i].addEventListener('click', deleteTodo);
 		function deleteTodo() {
 			this.parentNode.remove();
 			counterItem.innerHTML = --amountItem;
-			
 		}
+
 		fa_square[i].addEventListener('click', changeCheckbox);
 		function changeCheckbox() {
-			this.parentNode.classList.toggle('fa-square-active');
-		};
+			this.parentNode.classList.toggle('checkbox-active');
+		}
 	}
-	
 }
