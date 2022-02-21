0f19c5c8b4019e35d9a6e638a5b4e782abbcfc58
RBcuber <igor.kirichenko.07@gmail.com>
2022-02-21 20:08:12 +0200

    fix part of the code with increment

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ const input = document.getElementById('todo__input-task');
 const form = document.getElementById('todo__form');
 const list = document.querySelector('#todo__list');
 let test;
+let amountItem = 0;
 
 form.addEventListener('submit', qwe);
 
 function qwe(e) {
 	e.preventDefault();
-	if (input.value.length == 0) {
-	} else {
-		list.innerHTML += `
+	if (input.value.length) {
+			list.innerHTML += `
             <div class="todo__task-item">
 						
                 <div id="todo__task-name">
@@ -23,8 +23,8 @@ function qwe(e) {
 								</div>	
             </div>
 	        `;
-		input.value = '';
-		counterItem.innerHTML++;
+			input.value = '';
+			counterItem.innerHTML = ++amountItem;
 	}
 	let current_tasks = document.querySelectorAll('.todo__delete-item');
 	let fa_square = document.querySelectorAll('.fa-square');
@@ -32,12 +32,13 @@ function qwe(e) {
 		current_tasks[i].addEventListener('click', deleteTodo);
 		function deleteTodo() {
 			this.parentNode.remove();
-			counterItem.innerHTML--;
+			counterItem.innerHTML = --amountItem;
+			
 		}
 		fa_square[i].addEventListener('click', changeCheckbox);
 		function changeCheckbox() {
 			this.parentNode.classList.toggle('fa-square-active');
-			
 		};
 	}
+	
 }
