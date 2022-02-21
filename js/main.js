09b708f07458a30155542425ef3a9a37035a1d36
RBcuber <igor.kirichenko.07@gmail.com>
2022-02-21 19:41:31 +0200

    rename class

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
-let counterItem = document.getElementsByClassName('todo__counter')[0];
-let button = document.getElementsByClassName('button')[0];
+const counterItem = document.getElementsByClassName('todo__counter')[0];
+const button = document.getElementsByClassName('button')[0];
+const input = document.getElementById('todo__input-task');
+const form = document.getElementById('todo__form');
+const list = document.querySelector('#todo__list');
+let test;
 
-document.onkeyup = function (e) {
-	if (e.key === 'Enter') {
-		if (document.querySelector('#todo__input-task').value.length == 0) {
-		} else {
-			document.querySelector('#todo__tasks').innerHTML += `
+form.addEventListener('submit', qwe);
+
+function qwe(e) {
+	e.preventDefault();
+	if (input.value.length == 0) {
+	} else {
+		list.innerHTML += `
             <div class="todo__task-item">
 						
                 <div id="todo__task-name">
 								<i class="fa-regular fa-square sss"></i>
-                    ${document.querySelector('#todo__input-task').value}
+                    ${input.value}
                 </div>
 								 <div class="todo__delete-item">
 									 <i class="fa-solid fa-xmark"></i>
 								</div>	
             </div>
 	        `;
-			document.querySelector('#todo__input-task').value = '';
-			counterItem.innerHTML++;
-			
-		}
-			let current_tasks = document.querySelectorAll('.todo__delete-item');
-			let fa_square = document.querySelectorAll('.fa-square');
-			for (let i = 0; i < current_tasks.length; i++) {
-				current_tasks[i].onclick = function () {
-					this.parentNode.remove();
-					counterItem.innerHTML--;
-				};
-				fa_square[i].onclick = function () {
-					this.parentNode.classList.toggle('fa-square-active');
-				}
-			}
+		input.value = '';
+		counterItem.innerHTML++;
 	}
-};
+	let current_tasks = document.querySelectorAll('.todo__delete-item');
+	let fa_square = document.querySelectorAll('.fa-square');
+	for (let i = 0; i < current_tasks.length; i++) {
+		current_tasks[i].addEventListener('click', deleteTodo);
+		function deleteTodo() {
+			this.parentNode.remove();
+			counterItem.innerHTML--;
+		}
+		fa_square[i].addEventListener('click', changeCheckbox);
+		function changeCheckbox() {
+			this.parentNode.classList.toggle('fa-square-active');
+			
+		};
+	}
+}
