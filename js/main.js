f509071359c8ea8e492f54ac7ee2f02d40cd4508
RBcuber <igor.kirichenko.07@gmail.com>
2022-02-22 18:23:58 +0200

    fixed comment

--- js/main.js
+++ js/main.js
@@ -1,48 +1,47 @@
-const counterItem = document.getElementsByClassName('todo__counter')[0];
+const counterItem = document.querySelectorAll('.info__counter > span')[0];
 const button = document.getElementsByClassName('button')[0];
-const input = document.getElementById('todo__input-task');
+const input = document.getElementById('form__input');
 const form = document.getElementById('todo__form');
 const list = document.querySelector('#todo__list');
-let test;
 let amountItem = 0;
 
-const todoItem = (params) => (`
-            <div class="todo__item">
+const todoItem = (params) => `
+            <div class="list__item">
 						
-                <div id="todo">
-								<i class="fa-regular fa-square sss checkbox"></i>
+                <div id="list">
+								<i class="list__checkbox fa-regular fa-square sss "></i>
                     ${params}
                 </div>
-								 <div class="todo__delete-item">
-									 <i class="fa-solid fa-xmark"></i>
+								 <div class="list__delete-item">
+									 <i class="list__delete-cross fa-solid fa-xmark"></i>
 								</div>	
             </div>
-	        `)
+	        `;
 
-form.addEventListener('submit', qwe);
+form.addEventListener('submit', addTodoItem);
 
 function addTodoItem(e) {
+	let isInputValue = input.value
 	e.preventDefault();
 
-	if (input.value.length) {
-		list.innerHTML += todoItem(input.value);
+	if (isInputValue.length) {
+		list.innerHTML += todoItem(isInputValue);
 		input.value = '';
 		counterItem.innerHTML = ++amountItem;
 	}
+	let currentTodo = document.querySelectorAll('.list__delete-item');
+	let checkbox = document.querySelectorAll('.list__checkbox');
 
-	let current_tasks = document.querySelectorAll('.todo__delete-item');
-	let fa_square = document.querySelectorAll('.checkbox');
-
-	for (let i = 0; i < current_tasks.length; i++) {
-		current_tasks[i].addEventListener('click', deleteTodo);
+	for (let i = 0; i < currentTodo.length; i++) {
+		currentTodo[i].addEventListener('click', deleteTodo);
 		function deleteTodo() {
 			this.parentNode.remove();
 			counterItem.innerHTML = --amountItem;
 		}
 
-		fa_square[i].addEventListener('click', changeCheckbox);
+		checkbox[i].addEventListener('click', changeCheckbox);
 		function changeCheckbox() {
-			this.parentNode.classList.toggle('checkbox-active');
+			this.parentNode.classList.toggle('list__checkbox-active');
 		}
 	}
 }
